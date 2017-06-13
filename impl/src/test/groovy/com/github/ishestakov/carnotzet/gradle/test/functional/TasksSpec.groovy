854d35c03a1ba6d9322b1c41024dd41bea8d973d
Illia Shestakov <ilyuhazp@gmail.com>
2017-06-13 18:11:06 +0300

    Adjust functional tests.

--- impl/src/test/groovy/com/github/ishestakov/carnotzet/gradle/test/functional/TasksSpec.groovy
+++ impl/src/test/groovy/com/github/ishestakov/carnotzet/gradle/test/functional/TasksSpec.groovy
@@ -1,38 +1,88 @@
 package groovy.com.github.ishestakov.carnotzet.gradle.test.functional
 
 import org.gradle.testkit.runner.GradleRunner
-import org.gradle.testkit.runner.TaskOutcome;
-import spock.lang.Specification;
+import org.gradle.testkit.runner.TaskOutcome
+import spock.lang.Specification
+import spock.lang.Unroll
 
 public class TasksSpec extends Specification {
 
 
+    def buildFile = new File("../example-carnotzet/build.gradle");
 
-	def buildFile = new File("../example-carnotzet/build.gradle");
+    def setup() {
+        stopContainers()
+    }
 
-	def "zetStart task execution"() {
-		when:
-			def result = GradleRunner.create()
-					.withProjectDir(buildFile.toPath().getParent().toFile())
-		.withArguments("install", "zetStart")
-		.withPluginClasspath()
-		.build();
+    @Unroll
+    def "zetStart task execution with Gradle #gradleVersion"() {
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(buildFile.toPath().getParent().toFile())
+                .withArguments("install", "zetStart")
+                .withPluginClasspath()
+                .withGradleVersion(gradleVersion)
+                .build();
 
-		then:
-			result.output.contains('Hello!');
-			result.task(":zetStart").outcome == TaskOutcome.SUCCESS;
-	}
+        then:
+        result.output.contains("Starting");
+        containerNames().size() == 6
+        result.task(":zetStart").outcome == TaskOutcome.SUCCESS;
+        where:
+        gradleVersion << ["3.3", "2.8"]
 
-	def "zetStop task execution"() {
-		when:
-		def result = GradleRunner.create()
-				.withProjectDir(buildFile.toPath().getParent().toFile())
-				.withArguments("install", "zetStop")
-				.withPluginClasspath()
-				.build();
+    }
 
-		then:
-		result.output.contains('Hello!');
-		result.task(":zetStop").outcome == TaskOutcome.SUCCESS;
-	}
+    @Unroll
+    def "zetStop task execution with Gradle #gradleVersion"() {
+        given:
+        def startBuild = GradleRunner.create()
+                .withProjectDir(buildFile.toPath().getParent().toFile())
+                .withArguments("install", "zetStart")
+                .withPluginClasspath()
+                .build();
+        expect:
+        startBuild.task(':zetStart').outcome == TaskOutcome.SUCCESS;
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(buildFile.toPath().getParent().toFile())
+                .withArguments("install", "zetStop")
+                .withPluginClasspath()
+                .withGradleVersion(gradleVersion)
+                .build();
+
+        then:
+        for (def containerName : containerNames()) {
+            result.output.contains("Stopping ${containerName} ... done");
+        }
+        result.task(":zetStop").outcome == TaskOutcome.SUCCESS;
+
+        where:
+        gradleVersion << ["3.3", "2.8"]
+    }
+
+    List<String> containerNames() {
+        def process = new ProcessBuilder()
+                .command("docker", "ps", "--filter", "name=examplecarnotzet", "--format", "{{.Names}}")
+                .start()
+        process.waitFor()
+        Scanner s = new Scanner(process.getInputStream(), "UTF-8")
+        return (s.useDelimiter("\\A").hasNext() ? s.next() : "").split("\n") as List
+    }
+
+    List<String> containerIds() {
+        def process = new ProcessBuilder()
+                .command("docker", "ps", "--filter", "name=examplecarnotzet", "-q")
+                .start()
+        process.waitFor()
+        Scanner s = new Scanner(process.getInputStream(), "UTF-8")
+        return (s.useDelimiter("\\A").hasNext() ? s.next() : "").split("\n") as List
+    }
+
+    def stopContainers() {
+        containerIds().stream().forEach({
+            new ProcessBuilder().command("docker", "stop", it).start().waitFor();
+        })
+    }
 }
